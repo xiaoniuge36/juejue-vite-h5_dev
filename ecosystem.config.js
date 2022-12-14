47b30adc1a48e58ab39f608cf3701b42b315f573
niuge <694838286@qq.com>
2022-12-14 15:47:28 +0800

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,17 @@
 module.exports = {
   apps: [
     {
-      name: 'juejue-vite-h5',
+      name: 'juejue-vite-h5_dev',
       script: 'juejue-vite-h5-server.js'
     },
   ],
   deploy: {
     production: {
       user: 'root',
-      host: '47.99.134.126',
-      ref: 'origin/master',
-      repo: 'git@git.zhlh6.cn:Nick930826/juejue-vite-h5.git',
-      path: '/workspace/juejue-vite-h5',
+      host: '116.204.107.20',
+      ref: 'origin/main',
+      repo: 'git@github.com:xiaoniuge36/juejue-vite-h5_dev.git',
+      path: '/workspace/juejue-vite-h5_dev',
       'post-deploy': 'git reset --hard && git checkout master && git pull && npm i --production=false && pm2 startOrReload ecosystem.config.js', // -production=false 下载全量包
       env: {
         NODE_ENV: 'production'
