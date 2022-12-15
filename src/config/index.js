87156a6056b606f35ae52a22cd76e2e8c6fe079e
niuge <694838286@qq.com>
2022-12-15 15:01:41 +0800

    update

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,3 @@
 const MODE = import.meta.env.MODE // 环境变量
 
-export const baseUrl = MODE == 'development' ? 'http://127.0.0.1:8066' : 'http://api.chennick.wang'
+export const baseUrl = MODE == 'development' ? 'http://127.0.0.1:8066' : 'http://116.204.107.20:8066'
